2d42fe15072dd5ec6cbb45ed88167cd933933c7a
zlfera <zlfera@gmail.com>
2019-01-05 17:30:11 +0800

    up

--- lib/zz_web/views/layout_view.ex
+++ lib/zz_web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule ZzWeb.LayoutView do
   use ZzWeb, :view
+
+  def get_img do
+    # url = "http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1"
+    # url = HTTPoison.get!(url).body |> Jason.decode!()
+    # url = url["images"] |> List.first()
+    # "http://s.cn.bing.net" <> url["url"]
+    b = HTTPoison.get!("https://touduyu.com").body
+    [url] = Floki.attribute(b, "body", "style")
+    url
+  end
 end
--- mix.exs
+++ mix.exs
@@ -42,8 +42,10 @@ defmodule Zz.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:httpoison, "~> 1.4"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.0", runtime: false}
+      {:distillery, "~> 2.0", runtime: false},
+      {:floki, "~> 0.20.4"}
     ]
   end
 
