9bb04e5455861c46ef3b64562954b40b5cf7644c
zlfera <zlfera@gmail.com>
2019-01-06 08:09:50 +0800

    up

--- lib/zz_web/views/layout_view.ex
+++ lib/zz_web/views/layout_view.ex
@@ -10,4 +10,24 @@ defmodule ZzWeb.LayoutView do
     [url] = Floki.attribute(b, "body", "style")
     url
   end
+
+  def get_text do
+    url = "http://m.wufazhuce.com/one/" |> HTTPoison.get!()
+    [_, _, _, _, _, _, {_, cookie}, _, _, _] = url.headers
+    body = url.body |> Floki.find("div .ui-content script")
+    [{_, _, [token]}] = body
+    [_, token] = Regex.split(~r{= '}, token)
+    [token, _] = ~r{';} |> Regex.split(token)
+
+    res =
+      HTTPoison.request!(
+        :post,
+        "http://m.wufazhuce.com/one/ajaxlist/" <> "0" <> "?_token=" <> token,
+        "",
+        [{"Cookie", cookie}]
+      )
+
+    [h | _] = Jason.decode!(res.body)["data"]
+    %{content: h["content"], text_authors: h["text_authors"]}
+  end
 end
