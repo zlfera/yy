7c58218b8b749d4a04fb108a81f8f3eaafd60c8c
zlfera <zlfera@gmail.com>
2019-01-08 11:44:26 +0800

    up

--- config/prod.exs
+++ config/prod.exs
@@ -10,10 +10,10 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :zz, ZzWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
+  http: [:inet6, port: System.get_env("PORT") || 5000, compress: true],
   secret_key_base: "${SECRET_KEY_BASE}",
   server: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "https://www.youmile.vip", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :zz, Zz.Repo,
