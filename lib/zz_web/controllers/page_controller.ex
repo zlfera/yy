4881f590e308f6b43b469c5becfc45ba471226aa
zlfera <zlfera@gmail.com>
2019-01-08 18:07:33 +0800

    up

--- lib/zz_web/controllers/page_controller.ex
+++ lib/zz_web/controllers/page_controller.ex
@@ -2,7 +2,10 @@ defmodule ZzWeb.PageController do
   use ZzWeb, :controller
 
   def index(conn, _params) do
-    conn = Plug.Conn.put_resp_header(conn, "cache_control", "public,max-age=3600")
-    render(conn, "index.html", c: conn)
+    conn =
+      conn
+      |> put_resp_header("cache-control", "public,max-age=3600")
+
+    render(conn, "index.html")
   end
 end
