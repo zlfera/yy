9fd5e41a48f5102f8dd429f9203ee07562459fa9
zlfera <zlfera@gmail.com>
2019-01-08 18:17:20 +0800

    up

--- lib/zz_web/router.ex
+++ lib/zz_web/router.ex
@@ -7,6 +7,7 @@ defmodule ZzWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :put_resp_header, ["cache-control", "max-age=3, public, must-revalidate"]
   end
 
   pipeline :api do
