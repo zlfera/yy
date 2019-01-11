86cf3bfd32e9e521581f1a8142c887711f688582
zlfera <zlfera@gmail.com>
2019-01-12 06:34:18 +0800

    up

--- lib/zz_web/router.ex
+++ lib/zz_web/router.ex
@@ -7,7 +7,7 @@ defmodule ZzWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    # plug Plugs.OnePlug
+    plug Plugs.OnePlug
   end
 
   pipeline :api do
