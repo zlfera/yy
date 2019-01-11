450738645c5cde9e547f7645e5fc92c9fb314dc4
zlfera <zlfera@gmail.com>
2019-01-11 09:16:11 +0800

    up

--- lib/zz_web/controllers/page_controller.ex
+++ lib/zz_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule ZzWeb.PageController do
   def index(conn, _params) do
     conn
     # |> put_resp_header("cache-control", "max-age=3600, public")
-    |> render("index.html")
+    # |> render("index.html")
+    |> redirect(to: "/index.html")
   end
 end
