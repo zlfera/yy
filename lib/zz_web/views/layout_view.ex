f01130559e85754e083aff7fb56d6075bd387425
zlfera <zlfera@gmail.com>
2019-01-12 08:33:45 +0800

    up

--- lib/zz_web/views/layout_view.ex
+++ lib/zz_web/views/layout_view.ex
@@ -2,7 +2,11 @@ defmodule ZzWeb.LayoutView do
   use ZzWeb, :view
 
   def get_text do
-    url = "./priv/static/index.html" |> File.read!()
-    Jason.decode!(url)
+    if File.exists?("./priv/static/index.html") do
+      url = "./priv/static/index.html" |> File.read!()
+      Jason.decode!(url)
+    else
+      %{"content" => "", "text_authors" => ""}
+    end
   end
 end
