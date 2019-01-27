c527a8f3308de3237dc730e7845e7a37f33e5b6b
zlfera <zlfera@gmail.com>
2019-01-27 09:37:30 +0800

    up

--- lib/zz_web/endpoint.ex
+++ lib/zz_web/endpoint.ex
@@ -2,46 +2,50 @@ defmodule ZzWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :zz
   use NewRelic.Phoenix.Transaction
 
-  socket "/socket", ZzWeb.UserSocket,
+  socket("/socket", ZzWeb.UserSocket,
     websocket: true,
     longpoll: false
+  )
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :zz,
     gzip: true,
-    only: ~w(css fonts images js favicon.ico robots.txt index.html background_image.jpg)
+    only: ~w(css fonts images js favicon.ico robots.txt index.html background_image.jpg ali.jpg)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.RequestId
-  plug Plug.Logger
+  plug(Plug.RequestId)
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(Plug.Session,
     store: :cookie,
     key: "_zz_key",
     signing_salt: "IEiGTnyw"
+  )
 
-  plug ZzWeb.Router
+  plug(ZzWeb.Router)
 end
