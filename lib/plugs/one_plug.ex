4fa73757b7f647416bea2ba86b27afe6746784cd
zlfera <zlfera@gmail.com>
2019-03-03 23:28:12 +0800

    up

--- lib/plugs/one_plug.ex
+++ lib/plugs/one_plug.ex
@@ -4,6 +4,9 @@ defmodule Plugs.OnePlug do
 
   def call(conn, _opts) do
     conn
-    |> put_resp_header("strict-transport-security", "max-age=31536000; includeSubDomains")
+    |> put_resp_header(
+      "strict-transport-security",
+      "max-age=31536000; includeSubDomains; preload"
+    )
   end
 end
