4bb8e74f38fbc896325045840c4ffda9b6a0a103
zlfera <zlfera@gmail.com>
2019-07-18 11:53:44 +0800

    up

--- lib/zz/task_grain.ex
+++ lib/zz/task_grain.ex
@@ -4,53 +4,62 @@ defmodule Zz.TaskGrain do
 
   def a(dqqq) do
     u = "http://59.55.120.113:8311/web/bidPriceSpecialWatch?specialNo=#{dqqq}&specialName=z"
-    # uu = "http://59.55.120.113:8311/trade/open/watchSpecial"
+
     uu = "http://59.55.120.113:8311/trade/biddingAbout/tradeRequestListWatch"
+    uuu = "http://59.55.120.113:8311/trade/biddingAbout/tradeRequestListTotalWatch"
     headers = [referer: u]
     options = [params: [specialNo: dqqq]]
-    {o, url} = HTTPoison.post(uu, "", headers, options)
+    {:ok, url} = HTTPoison.post(uuu, "", headers, options)
+    page_no = ceil(Jason.decode!(url.body)["total"] / 10)
 
-    if o == :ok do
-      url.body |> Jason.decode!()
-    else
-      a(dqqq)
+    for i <- 1..page_no do
+      options = [params: [specialNo: dqqq, pageNo: i, pageSize: 10]]
+      {o, url} = HTTPoison.post(uu, "", headers, options)
+
+      if o == :ok do
+        url.body |> Jason.decode!()
+      else
+        a(dqqq)
+      end
     end
   end
 
   def grain(y, yy) do
     dd = a(y)
 
-    trantype =
-      if yy == "S" do
-        "拍卖"
-      else
-        "采购"
-      end
-
-    Enum.each(dd["row"], fn d ->
-      latest_price =
-        if d["statusName"] == "流拍" do
-          "0"
+    Enum.each(dd, fn dd ->
+      trantype =
+        if yy == "S" do
+          "拍卖"
         else
-          Integer.to_string(d["matchPrice"])
+          "采购"
         end
 
-      attr = %{
-        market_name: "guojia",
-        mark_number: d["REQUESTALIAS"],
-        year: "0",
-        variety: d["VARIETYNAME"],
-        grade: d["GRADENAME"],
-        trade_amount: Integer.to_string(d["NUM"]),
-        starting_price: Integer.to_string(d["PRICE"]),
-        latest_price: latest_price,
-        address: d["BUYDEPOTNAME"],
-        status: d["statusName"],
-        trantype: trantype
-      }
+      Enum.each(dd["row"], fn d ->
+        latest_price =
+          if d["statusName"] == "流拍" do
+            "0"
+          else
+            Integer.to_string(d["matchPrice"])
+          end
 
-      changeset = G.changeset(%G{}, attr)
-      Repo.insert(changeset)
+        attr = %{
+          market_name: "guojia",
+          mark_number: d["REQUESTALIAS"],
+          year: "0",
+          variety: d["VARIETYNAME"],
+          grade: d["GRADENAME"],
+          trade_amount: Integer.to_string(d["NUM"]),
+          starting_price: Integer.to_string(d["PRICE"]),
+          latest_price: latest_price,
+          address: d["BUYDEPOTNAME"],
+          status: d["statusName"],
+          trantype: trantype
+        }
+
+        changeset = G.changeset(%G{}, attr)
+        Repo.insert(changeset)
+      end)
     end)
   end
 end
